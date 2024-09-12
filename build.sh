29ce5b01f65f014fec6066ecf1a527848fb6fc2c
Dimitri Kandassamy <dimitri.kandassamy@gmail.com>
2024-09-12 22:53:58 +0200

    chore: Add argument parsing, usage message display and refactor into functions.
    Signed-off-by: Dimitri Kandassamy <dimitri.kandassamy@gmail.com>

--- build.sh
+++ build.sh
@@ -1,25 +1,69 @@
 #!/usr/bin/env bash
+#
+# This script loads the configuration from radar.config
+# and generate an index.html file by replacing placeholder 
+# text in template.html with the values from the configuration
 
 set -euo pipefail
 
-CONFIG_FILE="radar.config"
+# shellcheck disable=SC2155
+readonly PROG_NAME=$(basename "$0")
+readonly ARGS="$@"
 
-if [ -f "$CONFIG_FILE" ]; then
-    # shellcheck source=radar.config
-    # shellcheck disable=SC1091
-    . "${CONFIG_FILE}"
-else 
-    echo "[ERROR] Couldn't find the $CONFIG_FILE configuration file"
-    echo "[INFO] Please make sure the $CONFIG_FILE exists"
-    exit 1
-fi
+usage() {
+    cat <<- EOF
+$PROG_NAME - command line generator for technology radar  
 
-main() {
+Usage:
+
+    $PROG_NAME [flags]
+
+$PROG_NAME is a tool for generating an index.html file for the technology radar 
+based on a config file.
+
+Flags: 
+    -h, --help      Show help
+
+EOF
+}
+
+read_command_line() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+        -h | --help )
+            usage
+            exit 0
+            ;;
+        -* )
+            printf "flag provided but not defined: %s\n\n" "$1" >&2
+            usage
+            exit 1
+            ;;
+        esac 
+    done
+}
+
+load_configuration() {
+    local config="radar.config"
+
+    if [ -f "$config" ]; then
+        # shellcheck source=radar.config
+        # shellcheck disable=SC1091
+        . "${config}"
+    else 
+        echo "[ERROR] Couldn't find the $config configuration file"
+        echo "[INFO] Please make sure the $config exists"
+        exit 1
+    fi
+}
+
+create_index_html() {
+        # build statements
     local sed_statements=("-e s@\[ORG_NAME\]@$ORG_NAME@g")
     sed_statements+=("-e s@\[BASE_URL\]@$BASE_URL@g")
     sed_statements+=("-e s@\[DATE\]@$DATE@g")
 
-    
+    local i
     for ((i = 0; i < ${#QUADRANTS[@]}; i++)); do 
         sed_statements+=("-e s@\[QUADRANT_$i\]@${QUADRANTS[$i]}@g")
     done
@@ -33,9 +77,22 @@ main() {
     mv -i "index.tmp" "index.html" 
 }
 
+main() {
+    read_command_line "$ARGS"
+
+    load_configuration
+
+    create_index_html
+
+    exit 0
+}
+
+cleanup() {
+    # shellcheck disable=SC2317
+    rm -f -- index.tmp
+}
+
 # Cleanup on exit 
-trap "{ rm -f -- index.tmp;}" EXIT
+trap cleanup EXIT
 
 main
-
-exit 0
