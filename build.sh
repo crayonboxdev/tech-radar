327527fef65387456feafc2001597e8012443337
Dimitri Kandassamy <dimitri.kandassamy@gmail.com>
2024-09-12 13:21:18 +0200

    feat: Add tech radar index.html generation based on template and config
    This adds the files to generate the radar, the configuration logic and a script to generate the radar.
    
    - build.sh: use sed to create the index file from a config and template
    - radar.config.example: configuration example
    - template.html: template file
    
    Signed-off-by: Dimitri Kandassamy <dimitri.kandassamy@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -9,9 +9,33 @@ if [ -f "$CONFIG_FILE" ]; then
     # shellcheck disable=SC1091
     . "${CONFIG_FILE}"
 else 
-    echo "Couldn't find the $CONFIG_FILE configuration file"
-    echo "Please make sure the $CONFIG_FILE exists"
+    echo "[ERROR] Couldn't find the $CONFIG_FILE configuration file"
+    echo "[INFO] Please make sure the $CONFIG_FILE exists"
     exit 1
 fi
+
+main() {
+    local sed_statements=("-e s@\[ORG_NAME\]@$ORG_NAME@g")
+    sed_statements+=("-e s@\[BASE_URL\]@$BASE_URL@g")
+    sed_statements+=("-e s@\[DATE\]@$DATE@g")
+
+    
+    for ((i = 0; i < ${#QUADRANTS[@]}; i++)); do 
+        sed_statements+=("-e s@\[QUADRANT_$i\]@${QUADRANTS[$i]}@g")
+    done
+
+    for ((i = 0; i < ${#RINGS[@]}; i++)); do 
+        sed_statements+=("-e s@\[RING_$i\]@${RINGS[$i]}@g")
+    done
+
+    sed "${sed_statements[@]}" template.html > index.tmp
+
+    mv -i "index.tmp" "index.html" 
+}
+
+# Cleanup on exit 
+trap "{ rm -f -- index.tmp;}" EXIT
+
+main
 
 exit 0
